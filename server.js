588c4bafe480abeb7b66371a34a54fe066be23a6
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-04-21 21:43:59 +0200

    server+procfile

--- server.js
+++ server.js
@@ -3,12 +3,34 @@
  */
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = 80;
 
 app.all( '*' , function(request, response){
     response.status(404);
     response.send('404 - Not found.');
 })
+
+app.get('/json', function(request, response) {
+    response.json({
+        'some_name': 'Value',
+        'an_array_of_objects': [
+            {
+                'another_name': 'Another value',
+                'a_further_name': "A further value"
+            },
+            {'yet_another_name': 'Yet another value'}
+        ],
+        'some_boolean': true,
+        'some_integer': 42,
+        'array_of_ints': [
+            2, 3, 5, 7, 11, 13
+        ],
+        'array_of_strings': [
+            "twee", "drie", "vijf", "zeven"
+        ]
+    })
+})
+
 
 app.get( '/' , function(request, response) {
     response.send('Bonjour.');
